5b3724a53a0e1df17c21fcc3d3091906e8b2f76c
wangjianing <jianingmica@gmail.com>
2023-10-17 17:16:48 +0200

    feat: heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,14 +3,19 @@ import { Component } from '@angular/core';
 // interface
 import { Hero } from '../hero';
 
+// mock data
+import { HEROES } from '../mock-heros';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes: Hero[] = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
